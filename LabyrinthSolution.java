f9871562c652bd94a738c2433da261bd8b7ab0d9
Nate Wilkes <nwilkes@friendsbalt.org>
2021-12-04 23:44:48 -0500

    Done

--- LabyrinthSolution.java
+++ LabyrinthSolution.java
@@ -11,7 +11,7 @@ public class LabyrinthSolution{
 	static boolean[][] visited;
 	
 	/**
-	/ Makes safe moves to solve the maze and records the moves in the ArrayList
+	/ Makes moves to solve the maze and records the moves in the ArrayList
 	/ @param l the maze that will be solved
 	/ @param row the curremy y coordinate
 	/ @param column the current x coordinate
@@ -42,8 +42,16 @@ public class LabyrinthSolution{
 		}
 		return solved;
 	}
-
+ 
+ /**
+ / Determines whether or not there is a safe move available, if there isn't it begins backtracking
+ / @param l the maze that is being solved
+ / @param row the current y coordinate
+ / @param column the current x coordinate
+ / @return whether or not a safe move is available
+ */
 	static boolean isSafeMove(Labyrinth l, int row, int column){
+		//checks to see if a safe move is possible
 		if(isSafe(l, row + 1, column))
 			return true;
 		else if(isSafe(l, row, column + 1))
@@ -53,6 +61,7 @@ public class LabyrinthSolution{
 		else if(isSafe(l, row, column - 1))
 			return true;
 		
+		//backtracks
 		if(moves.get(moves.size() - 1) == 0){
 			moves.remove(moves.size() - 1);
 			return isSafeMove(l, row + 1, column);
@@ -70,7 +79,15 @@ public class LabyrinthSolution{
 			return isSafeMove(l, row, column - 1);
 		}
 	}
+
 	
+	/**
+	/ Checks to see if a move is safe
+	/ @param l the labyrinth that is being solved
+	/ @param row the y coordinate that is being checked
+	/ @param column the x coordinate that is being checked
+	/ @return whether or not the move is safe/legal
+	*/
 	static boolean isSafe(Labyrinth l, int row, int column){
 		if(l.isValid(row, column) && l.isStone(row, column)){
 			if(row < visited.length && column < visited[0].length){
@@ -81,6 +98,11 @@ public class LabyrinthSolution{
 		return false;
 	}
 	
+	/**
+	/ Converts the ArrayList into an to send to the Labyrinth method
+	/ @param l the labyrinth that has been solved and contains the ArrayList with the moves
+	/ @return the converted array
+	*/
 	static int[] toSolution(Labyrinth l){
 		int[] solution = new int[moves.size()];
 		for(int i = 0; i < moves.size(); i++){
@@ -99,20 +121,10 @@ public class LabyrinthSolution{
 		System.out.println(args[0]);
 		System.out.println(args[1]);
 		
-		
 		boolean test = solveMaze(l, 0, 0);
 		
 		System.out.println(test);
 		int[] solution = toSolution(l);
 		System.out.println(l.solves(solution));
-		
-		l.printGrid();
-		for(int i = 0; i < visited.length; i++){
-			for(int j =0; j < visited[0].length; j++)
-				System.out.print(visited[i][j] + " ");
-			System.out.println();
-		}
-		for(int i = 0; i < solution.length; i++)
-			System.out.print(solution[i] + " ");
 	}
 }
